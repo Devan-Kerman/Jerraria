f30076ea4701564313fb821a5840c4de5c192e40
Ramid Khan <ramidzkh@gmail.com>
2021-11-08 16:19:28 +1100

    prep for ssl

--- src/client/java/net/devtech/jerraria/client/network/TestClient.java
+++ src/client/java/net/devtech/jerraria/client/network/TestClient.java
@@ -15,11 +15,15 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.websocketx.WebSocketClientHandshakerFactory;
 import io.netty.handler.codec.http.websocketx.WebSocketVersion;
 import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketClientCompressionHandler;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import net.devtech.jerraria.server.network.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.net.URI;
+import java.security.cert.Certificate;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Supplier;
@@ -33,6 +37,12 @@ public class TestClient {
 		Supplier<EventLoopGroup> supplier = Nettyworking.select(NioEventLoopGroup::new, EpollEventLoopGroup::new, KQueueEventLoopGroup::new);
 		EventLoopGroup group = supplier.get();
 
+		// TLS encryption
+		boolean isEncrypted = false;
+
+		// Certificate validation to verify server's identity
+		boolean isSecure = false;
+
 		WebSocketClientHandshakeHandler handler = new WebSocketClientHandshakeHandler(WebSocketClientHandshakerFactory.newHandshaker(URI.create("ws://localhost:8008/help_me"), WebSocketVersion.V13, null, true, EmptyHttpHeaders.INSTANCE));
 
 		try {
@@ -43,12 +53,17 @@ public class TestClient {
 				.option(ChannelOption.TCP_NODELAY, true)
 				.handler(new ChannelInitializer<SocketChannel>() {
 					@Override
-					protected void initChannel(@NotNull SocketChannel channel) {
+					protected void initChannel(@NotNull SocketChannel channel) throws Exception {
+						channel.pipeline().addLast("timeout", new ReadTimeoutHandler(30));
+
+						if (isEncrypted) {
+							channel.pipeline().addLast("ssl", SslContextBuilder.forClient()
+								.trustManager(isSecure ? null : InsecureTrustManagerFactory.INSTANCE)
+								.build()
+								.newHandler(channel.alloc(), "localhost", 8008, channel.eventLoop()));
+						}
+
 						channel.pipeline()
-							.addLast("timeout", new ReadTimeoutHandler(30))
-							// only when wss scheme is used
-							// use null instead of insecure when we want encryption + verification
-							// .addLast(SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build().newHandler(channel.alloc(), group))
 							.addLast("http", new HttpClientCodec())
 							.addLast("http_aggregator", new HttpObjectAggregator(8192))
 							.addLast("compression", WebSocketClientCompressionHandler.INSTANCE)
@@ -59,7 +74,14 @@ public class TestClient {
 							.addLast("splitter", new Pagination())
 							.addLast("connection", new ChannelInboundHandlerAdapter() {
 								@Override
-								public void channelActive(@NotNull ChannelHandlerContext ctx) {
+								public void channelActive(@NotNull ChannelHandlerContext ctx) throws Exception {
+									if (ctx.pipeline().get("ssl") instanceof SslHandler ssl) {
+										Certificate[] certificates = ssl.engine().getSession().getPeerCertificates();
+										Certificate certificate = certificates[certificates.length - 1];
+
+										System.out.println("Their certificate: " + certificate);
+									}
+
 									ctx.fireChannelActive();
 									ctx.writeAndFlush(new PacketCodec.Packet(1, ctx.alloc().buffer().writeBytes(data)));
 								}
@@ -80,7 +102,7 @@ public class TestClient {
 							});
 					}
 				})
-				.connect("::1", 8008)
+				.connect("localhost", 8008)
 				.sync()
 				.channel();
 			channel.closeFuture().sync();
