e792136c906bbc2705dd27a580e3a64a154252da
Devan-Kerman <dev.sel20@gmail.com>
2022-04-21 17:22:17 -0500

    4/21 - LT

--- src/main/java/net/devtech/jerraria/jerraria/tile/AirTile.java
+++ src/main/java/net/devtech/jerraria/jerraria/tile/AirTile.java
@@ -1,13 +1,8 @@
 package net.devtech.jerraria.jerraria.tile;
 
-import net.devtech.jerraria.util.math.Matrix3f;
-import net.devtech.jerraria.world.World;
 import net.devtech.jerraria.world.tile.Tile;
-import net.devtech.jerraria.world.tile.TileData;
 import net.devtech.jerraria.world.tile.TileVariant;
-import net.devtech.jerraria.world.tile.render.ShaderSource;
 import net.devtech.jerraria.world.tile.render.TileRenderer;
-import org.jetbrains.annotations.Nullable;
 
 public class AirTile extends Tile {
 	static final TileRenderer NO_OP = (source, tileMatrix, localWorld, variant, clientTileData, x, y) -> {
@@ -20,4 +15,5 @@ public class AirTile extends Tile {
 	public TileRenderer getRenderer(TileVariant variant) {
 		return NO_OP;
 	}
+
 }
--- src/main/java/net/devtech/jerraria/world/entity/BaseEntity.java
+++ src/main/java/net/devtech/jerraria/world/entity/BaseEntity.java
@@ -2,16 +2,11 @@ package net.devtech.jerraria.world.entity;
 
 import net.devtech.jerraria.jerracode.element.JCElement;
 import net.devtech.jerraria.util.math.SimpleShape;
-import net.devtech.jerraria.world.ChunkLinkingAccess;
 import net.devtech.jerraria.world.EntitySearchType;
 import net.devtech.jerraria.world.World;
 
-import static java.lang.Math.*;
-
 public abstract class BaseEntity extends Entity {
 	SimpleShape bounds;
-	int fx, fy, tx = -1, ty;
-	boolean linkedLocal;
 
 	public BaseEntity(Type<?> type, SimpleShape bounds) {
 		super(type);
@@ -39,46 +34,7 @@ public abstract class BaseEntity extends Entity {
 	protected void tick() {
 		// move entity if linked properly, we do this in beginning because the entity changes
 		super.tick();
-	}
-
-	@Override
-	protected void remove() {
-		int oldX = this.oldChunkX, oldY = this.oldChunkY;
-		World oldWorld = this.world;
-		super.remove();
-		if(oldWorld != null && this.linkedLocal) {
-			ChunkLinkingAccess unlink = oldWorld.getUnsafeUnlinkingAccess(oldX, oldY);
-			unlink.range(this.fx, this.fy, this.tx, this.ty);
-			this.linkedLocal = false;
-		}
-	}
-
-	@Override
-	boolean tickPosition() {
-		int oldX = this.oldChunkX, oldY = this.oldChunkY;
-		World oldWorld = this.world;
-		boolean moved = super.tickPosition();
-		if(moved) {
-			if(this.linkedLocal) {
-				ChunkLinkingAccess unlink = oldWorld.getUnsafeUnlinkingAccess(oldX, oldY);
-				unlink.range(this.fx, this.fy, this.tx, this.ty);
-			}
-
-			this.fx = this.getBlockX();
-			this.fy = this.getBlockY();
-			SimpleShape.Rectangle tx = this.bounds.maxBounds();
-			this.tx = (int) ceil(tx.width() + fx);
-			this.ty = (int) ceil(tx.height() + fy);
-		}
-		return moved;
-	}
-
-	@Override
-	void afterRelocation(World world) {
-		super.afterRelocation(world);
-		ChunkLinkingAccess link = this.world.getUnsafeLinkingAccess(this.oldChunkX, this.oldChunkY);
-		link.range(this.fx, this.fy, this.tx, this.ty);
-		this.linkedLocal = true;
+		
 	}
 
 	protected SimpleShape getBounds() {
