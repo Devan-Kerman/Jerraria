6aacc22dde83cf5bcd5e7784e1873c5ed620cdc6
ueaj <dev.sel20@gmail.com>
2022-07-13 19:30:05 -0600

    save

--- jerraria-rendering/src/main/java/net/devtech/jerraria/util/math/Mat4f.java
+++ jerraria-rendering/src/main/java/net/devtech/jerraria/util/math/Mat4f.java
@@ -1,7 +1,13 @@
 package net.devtech.jerraria.util.math;
 
+import java.nio.FloatBuffer;
+import java.util.concurrent.atomic.AtomicReference;
+
+import net.devtech.jerraria.render.api.base.GlData;
+
 import net.minecraft.util.math.Matrix4f;
 import net.minecraft.util.math.Quaternion;
+import net.minecraft.util.math.Vector4f;
 
 public final class Mat4f extends Mat {
 	final Matrix4f mat;
@@ -48,41 +54,73 @@ public final class Mat4f extends Mat {
 
 	@Override
 	public MatType getType() {
-		return this;
+		return MatType.MAT4;
 	}
 
 	@Override
 	public Mat copy() {
-		return this;
+		return new Mat4f(this.mat.copy());
 	}
 
 	@Override
 	public Mat inverse(Mat mat) {
-		return this;
+		Matrix4f copy = this.mat.copy();
+		copy.invert();
+		return new Mat4f(copy);
+	}
+
+	public Vector4f transform(float x, float y, float z, float w) {
+		Vector4f vector = new Vector4f(x, y, z, w);
+		vector.transform(this.mat);
+		return vector;
 	}
 
 	@Override
 	public float mulX(float x, float y, float z, float w) {
-		return 0;
+		return this.transform(x, y, z, w).getX();
 	}
 
 	@Override
 	public float mulY(float x, float y, float z, float w) {
-		return 0;
+		return this.transform(x, y, z, w).getY();
 	}
 
 	@Override
 	public float mulZ(float x, float y, float z, float w) {
-		return 0;
+		return this.transform(x, y, z, w).getZ();
 	}
 
 	@Override
 	public float mulW(float x, float y, float z, float w) {
-		return 0;
+		return this.transform(x, y, z, w).getW();
 	}
 
 	@Override
-	public float getElement(int x, int y) {
-		return 0;
+	public void upload(GlData.Buf buf, int m, int n) {
+		if(buf instanceof GlData.FlushableBuf f) {
+			FloatBuffer fb = f.fb();
+			this.mat.writeColumnMajor(fb);
+			f.flush0();
+		} else {
+			FloatBuffer temp = FloatBuffer.allocate(16);
+			this.mat.writeColumnMajor(temp);
+			for(int i = 0; i < temp.position(); i++) {
+				buf.f(temp.get(i));
+			}
+		}
+	}
+
+	@Override
+	public float getElement(int x, int y) { // extremely unperformant
+		FloatBuffer buffer = FloatBuffer.allocate(16);
+		this.mat.writeColumnMajor(buffer);
+		return buffer.get(x*4+y);
+	}
+
+	public static class Type extends MatType {
+		@Override
+		public Mat createIdentity() {
+			return new Mat4f();
+		}
 	}
 }
